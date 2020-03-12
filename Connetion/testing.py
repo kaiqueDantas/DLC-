2708bee93dfa83a4d6555072e25fee73e6d837e7
Kaique Dantas <cursocomputacoes@gmail.com>
2020-03-12 13:56:01 -0300

    Ajuste das funções de conexão com o banco de dados

--- Connetion/testing.py
+++ Connetion/testing.py
@@ -6,43 +6,72 @@ class dba:
                                  passwd='',)
     cursor = connection.cursor()
 
-    def dataBase(self):
+    def dataBase(self, banco):
+        '''Função destinada a criação do banco de dados'''
 
         try:
             cursor = self.connection.cursor()
-            cursor.execute('create database DLC')
+            cursor.execute(f'create database {banco}')
 
         except Exception as e:
-            print('ERROR IN DBA: ',e)
+            print('Error message in the database : ',e)
 
-    def update(self):
+    def update(self, query):
+
+        '''Função destinada a alteração de dados '''
         try:
 
-            self.cursor.execute()
+            self.cursor.execute(query)
 
         except Exception as e:
-            print('ERROR IN DBA: ', e)
+            print('Error message in the database : ',e)
 
-    def create(self):
+    def create(self, query):
         try:
 
-            self.cursor.execute()
+            self.cursor.execute(query)
 
         except Exception as e:
-            print('ERROR IN DBA: ', e)
-    def delete(self):
+            print('Error message in the database : ',e)
+
+    def delete(self, query):
         try:
 
-            self.cursor.execute()
+            self.cursor.execute(query)
 
         except Exception as e:
-            print('ERROR IN DBA: ', e)
+            print('Error message in the database : ',e)
+
+    def select(self, query):
+        try:
+
+            self.cursor.execute(query)
+
+        except Exception as e:
+            print('Error message in the database : ', e)
+
     def close(self):
         try:
 
             self.connection.close()
 
         except Exception as e:
-            print('ERROR IN DBA: ', e)
+            print('Error message in the database : ',e)
+
+    def showDatabase(self):
+        '''
+        Este método verifica a extência de todos os bancos de dados
+        '''
+
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute('SHOW DATABASES')
+
+            for x in cursor:
+                print(x)
+
+        except Exception as e:
+            print('Error message in the database : ',e)
+
 
 
--- Main/Main.py
+++ Main/Main.py
@@ -6,8 +6,30 @@
 if __name__ == '__main__':
     from Connetion import testing
     conenect = testing.dba()
-    #conenect.dataBase()
+    conenect.select()
     conenect.close()
+
+
+def create():
+
+    '''Função destinada a passar a query para a função da classe dba '''
+
+    query = '''CREATE TABLE id_dlc (
+    id_dlc int (100000)not null ,
+    tipo int (100) not null);
+    
+    create table dlc (
+	    id_dlc int not null,
+        cod_dlc int not null,
+        nome_dlc int not null); 
+        '''
+    conenect = testing.dba()
+    conenect.create(query)
+    conenect.close()
+
+
+
+
 
 
 
